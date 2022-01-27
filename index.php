da5583f7119ec4d2805211aa7bf5510271552673
{tatsuya-m-nisato@outlook.jp} <tatsuya-m-nisato@outlook.jp>
2022-01-27 10:58:57 +0900

    setting the format

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+ <html lang="ja">
+  <head>
+   <meta charset="UTF-8">
+   <title>HTML Sample</title>
+   <link rel="stylesheet" href="stylesheet.css">
+   <meta name="description" content="ディスクリプションを入力">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <header>
+      <div class="container">           </div>
+    </header>
+    <footer>
+      <div class="container">           </div>
+    </footer>
+    <script src="jquery-3.6.0.min.js"></script>
+    <script src="script.js"></script>
+  </body>
+ </html>
