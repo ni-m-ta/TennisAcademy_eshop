e2b9818a2b5e735f3bb54108eaec99906e887427
{tatsuya-m-nisato@outlook.jp} <tatsuya-m-nisato@outlook.jp>
2022-01-28 13:15:10 +0900

    creating the header

--- home.php
+++ home.php
@@ -2,20 +2,42 @@
  <html lang="ja">
   <head>
    <meta charset="UTF-8">
-   <title>Tennis Academy</title>
+   <title>Tennis Academy E-shop</title>
    <link rel="stylesheet" href="stylesheet.css">
-   <meta name="description" content="当サイトはe-Cafeの注文サイトとなっております。">
+   <meta name="description" content="当サイトは本アカデミーの公式物品販売サイトです。">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <script src="https://kit.fontawesome.com/00f67a9916.js" crossorigin="anonymous"></script>
   </head>
   <body>
-    <div class="menu-wrapper container">
-      <h1>e-Cafe</h1>
-      <form method="post" action="confirm.php">
-        <div clas="menu-items">
+    <header>
+      <div class="container">
+        <div class="header-upper">
+          <img class="header-logo" src="C:\xampp\htdocs\TennisAcademy_eshop\images\TennisAcadmy_logo.png">
+          <h3 class="sport-name">Tennis</h3>
+          <form action="localhost/TennisAcady_eshop" method="get">
+            <input type="search" name="search" placeholder="キーワードを入力">
+            <input type="submit" name="submit" value="検索">
+          </form>
+          <i class="fab fa-facebook-square"></i>
+          <i class="fab fa-twitter-square"></i>
         </div>
-        <input type="submit" value="注文する">
-      </form>
+        <div class="header-below">
+          <a class="header-option">NEWS</a>
+          <a class="header-option">PRODUCTS</a>
+          <a class="header-option">PLAYERS</a>
+          <a class="header-option">INNOVATION</a>
+          <a class="header-option">VIDEOS</a>
+          <a class="header-option">EVENTS</a>
+        </div>
+      </div>
+    </header>
+    <div class="menu-wrapper container">
     </div>
+    <footer>
+      <div class="container">
+
+      </div>
+    </footer>
     <script src="jquery-3.6.0.min.js"></script>
     <script src="script.js"></script>
   </body>
